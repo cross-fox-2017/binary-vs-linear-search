b6fd48e590ad0e8fb8b876d8f200253cce970c70
Syanmil <syanmil@yahoo.com>
2017-01-03 15:19:28 +0700

    menyesuaikan dari documentasi, fungsi test belum masuk

--- Benchmark.js
+++ Benchmark.js
@@ -2,7 +2,18 @@ var Benchmark = require('benchmark')
 var suite = new Benchmark.Suite;
 
 // add tests
+suite.add('Linier#test', function() {
 
+})
+.add('Binary#test', function() {
+
+})
 // add listeners
-
+.on('cycle', function(event) {
+  console.log(String(event.target));
+})
+.on('complete', function() {
+  console.log('Fastest is ' + this.filter('fastest').map('name'));
+})
 //
+.run({'async': false})
