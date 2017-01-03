d5cb81de7e1f911e9a6d3cb75e5d76cbf71e6e4a
Windi <windi@hacktiv8.com>
2017-01-03 16:16:18 +0700

    add skeleton

---
+++ benchmark.js
@@ -0,0 +1,6 @@
+"use strict";
+
+var Benchmark = require('benchmark');
+var search = require('./binary_linear');
+
+// benchmark code here
---
+++ binary_linear.js
@@ -0,0 +1,13 @@
+"use strict";
+
+var search = {};
+
+search.linearSearch = function(values, target){
+  //linear search code here
+}
+
+search.binarySearch = function(values, target){
+  //binary search code here
+}
+
+module.exports = search;
